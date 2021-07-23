b41b34b225698d90057120fd8649e5e0aae38f7c
hmooz-6 <77557916+hmooz-6@users.noreply.github.com>
2021-07-23 23:32:02 +0300

    Update vc.py

--- vc.py
+++ vc.py
@@ -1,7 +1,7 @@
 #Bovonos
 import threading ,amino, requests, json
 
-client=amino.Client(deviceId="01C7B3C348050279D37873F25333E810A2975EC623153D5B0505679B4F1746425A1E3B4004B898369B")
+client=amino.Client()
 print ("By Bovonos")
 client.login(email=input("Email: "),password="hamza#$1234")
 nfo=client.get_from_code('http://aminoapps.com/p/hhxbvm')
